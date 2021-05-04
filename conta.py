5cac57af7a9b15fa1492ac926d232b0be6625699
Yuri <yuricoelho33@gmail.com>
2021-05-04 15:26:44 -0300

    Adicionando tratamento no método saca e criando um método privado que verifica se é possível sacar

--- conta.py
+++ conta.py
@@ -6,6 +6,7 @@ class Conta:
         self.__titular = titular
         self.__saldo = saldo
         self.__limite = limite
+        self.__codigo_banco = "001"
 
     def extrato(self):
         print("Saldo {} do titular {} ".format(self.__saldo, self.__titular))
@@ -13,8 +14,15 @@ class Conta:
     def deposita(self, valor):
         self.__saldo += valor
 
+    def __pode_sacar(self, valor_a_sacar):
+        valor_disponivel_a_sacar = self.__saldo + self.__limite
+        return valor_a_sacar <= valor_disponivel_a_sacar
+
     def saca(self, valor):
-        self.__saldo -= valor
+        if(self.__pode_sacar(valor)):
+            self.__saldo -= valor
+        else:
+            print("O valor {} passou do limite".format(valor))
 
     def transfere(self, valor, destino):
         self.saca(valor)
