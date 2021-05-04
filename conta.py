82e197af7a240fefe7bd28e86518e2ee59fa05ff
Yuri <yuricoelho33@gmail.com>
2021-05-04 15:49:26 -0300

    Adicionando métodos estáticos

--- conta.py
+++ conta.py
@@ -6,7 +6,6 @@ class Conta:
         self.__titular = titular
         self.__saldo = saldo
         self.__limite = limite
-        self.__codigo_banco = "001"
 
     def extrato(self):
         print("Saldo {} do titular {} ".format(self.__saldo, self.__titular))
@@ -41,3 +40,11 @@ class Conta:
     @limite.setter
     def limite(self, limite):
         self.__limite = limite
+
+    @staticmethod
+    def codigo_banco():
+        return "001"
+
+    @staticmethod
+    def codigos_bancos():
+        return {'BB': '001', 'Caixa': '104', 'Bradesco': '237'}
